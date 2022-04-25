902e40ad43cf6315409a67515718bf706d2ecff1
TheBrilliantAsad <53337734+AsadGohar@users.noreply.github.com>
2022-04-25 16:59:39 +0500

    updates to Admin Panel

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,12 +1,112 @@
 import React from 'react'
-import { contract,isPaused} from '../../utils/contractUtils'
+import { 
+  contract,
+  getPaused, 
+  getPreSaleCost, 
+  getPublicSaleCost, 
+  getRevealed, 
+  getTotalMinted,
+  setPauseContract,
+  setRevealContract,
+  setPreSaleCostContract,
+  setPublicSaleCostContract,
+  updateWhiteList,
+  mintByAdmin
+ } from '../../utils/contractUtils'
+import { useSelector } from 'react-redux'
 import './Admin.css'
 
 const Admin = () => {
+  
+  let {user}  = useSelector(state=>state.user)
+
+  const [mint,setMint] = React.useState(0)
+  const [isPaused,setIsPaused] = React.useState(false)
+  const [isRevealed,setIsRevealed] = React.useState(false)
+  const [publicSaleCost,setPublicSaleCost] = React.useState('')
+  const [preSaleCost,setPreSaleCost] = React.useState('') 
+  const [preSaleValue,setPreSaleValue] = React.useState('') 
+  const [publicSaleValue,setPublicSaleValue] = React.useState('') 
+  const [pauseValue,setPauseValue] = React.useState(true) 
+  const [revealValue,setRevealValue] = React.useState(false) 
+  const [whiteListAddress,setWhiteListAddress] = React.useState('')
+  const [baseUri,setBaseUri] = React.useState('')
+  const [totalMinted,setTotalMinted] = React.useState(false)
+  // const [isRevealed,setIsRevealed] = React.useState(false)
 
   React.useEffect(()=>{
-    console.log(isPaused())
+    // console.log(await getPaused())
+    setData()
   },[])
+
+  const mintNfts = async (e) => {
+    e.preventDefault()
+    mintByAdmin(mint,user)
+  }
+
+  const increaseMint = ()=> {
+    setMint(mint+1)
+ }
+
+ const decreaseMint = ()=> {
+     if(mint === 0){
+         setMint(0)
+     } else {
+         setMint(mint-1)
+     }
+ }
+
+  const setData = async () => {
+    setIsPaused(await getPaused())
+    setIsRevealed(await getRevealed())
+    setPublicSaleCost(await getPublicSaleCost())
+    setPreSaleCost(await getPreSaleCost())
+    // setTotalMinted(await getTotalMinted())
+  }
+
+  const pauseContract = (e) => {
+    e.preventDefault()
+    setPauseContract(pauseValue,user)
+    // setData()
+  }
+
+  const revealContract = (e) => {
+    e.preventDefault()
+    setRevealContract(revealValue,user)
+    // setData()
+  }
+  
+  const getPausedValue = async () => {
+    setIsPaused(await getPaused())
+  }
+
+  const getRevealedValue = async () => {
+    setIsRevealed(await getRevealed())
+  }
+
+  const setPreSaleValueAdmin = async () => {
+    setPreSaleCostContract(preSaleValue,user)
+  }
+
+  const setPublicSaleValueAdmin = async () => {
+    setPublicSaleCostContract(publicSaleValue,user)
+  }
+
+  const getPreSaleValueAdmin = async () => {
+    setPreSaleCost(await getPreSaleCost())
+  }
+
+  const getPublicSaleValueAdmin = async () => {
+    setPublicSaleCost(await getPublicSaleCost())
+  }
+
+  const addUserToWhiteList = () => {
+    updateWhiteList(whiteListAddress,user)
+  }
+
+  const setBaseUriContract = async () => {
+    setBaseUri(baseUri,user)
+  }
 
   return (
     <div id='admin' className='container-fluid' >
@@ -14,76 +114,83 @@ const Admin = () => {
       <div className='row justify-content-center'>
         <div className='col-md-7' >
           <div className="input-group mb-3 px-4">
-            <input type="text" className="form-control input-bg" placeholder="Whitelist Address" 
-            aria-label="Pre Sale Price" aria-describedby="button-addon2"/>
-            <button className="btn btn-outline-secondary btns" type="button" 
-            id="button-addon2">Add User To Whitelist</button>
+            <input value={whiteListAddress} onChange={e=>{setWhiteListAddress(e.target.value)}} type="text" className="form-control input-bg" placeholder="Whitelist Address" 
+              aria-label="Pre Sale Price" aria-describedby="button-addon2"/>
+            <button onClick={addUserToWhiteList} className="btn btn-outline-secondary btns" type="button" 
+              id="button-addon2">Add User To Whitelist</button>
           </div>
           <div className="input-group mb-3 px-4">
-            <input type="text" className="form-control input-bg" placeholder="Base URI" 
+            <input value={baseUri} onChange={e=>setBaseUri(e.target.value)} type="text" className="form-control input-bg" placeholder="Base URI" 
             aria-label="Public Sale Price" aria-describedby="button-addon2"/>
-            <button className="btn btn-outline-secondary btns" type="button" 
-            id="button-addon2">Update Base URI</button>
+            <button onClick={setBaseUri} className="btn btn-outline-secondary btns" type="button" 
+              id="button-addon2">Update Base URI</button>
           </div>
           <div className="input-group mb-3 px-4">
-            <input type="text" className="form-control input-bg" placeholder="Pre Sale Price" 
+            <input value={preSaleValue} onChange={e=>setPreSaleValue(e.target.value)} type="text" className="form-control input-bg" placeholder="Pre Sale Price" 
             aria-label="Pre Sale Price" aria-describedby="button-addon2"/>
-            <button className="btn btn-outline-secondary btns" type="button" 
-            id="button-addon2">Set Pre Sale Price</button>
+            <button onClick={setPreSaleValueAdmin} className="btn btn-outline-secondary btns" type="button" 
+              id="button-addon2">Set Pre Sale Price</button>
           </div>
           <div className="input-group mb-3 px-4">
-            <input type="text" className="form-control input-bg" placeholder="Public Sale Price" 
+            <input value={publicSaleValue} onChange={e=>setPublicSaleValue(e.target.value)} type="text" className="form-control input-bg" placeholder="Public Sale Price" 
             aria-label="Public Sale Price" aria-describedby="button-addon2"/>
-            <button className="btn btn-outline-secondary btns" type="button" 
-            id="button-addon2">Set Public Sale Price</button>
+            <button onClick={setPublicSaleValueAdmin} className="btn btn-outline-secondary btns" type="button" 
+              id="button-addon2">Set Public Sale Price</button>
           </div>
           <div className="input-group mb-3 px-4">
             <input type="text" className="form-control input-bg" placeholder="Unrevealed URI" 
             aria-label="Public Sale Price" aria-describedby="button-addon2"/>
             <button className="btn btn-outline-secondary btns" type="button" 
-            id="button-addon2">Set Unrevealed URI</button>
+              id="button-addon2">Set Unrevealed URI</button>
           </div>
           <div className="input-group mb-3 px-4">
               <button className="btn btn-outline-secondary btns" type="button" 
-              id="button-addon2">Set Paused</button>
-              <select className="form-select select-bg" id="inputGroupSelect03" aria-label="Example select with button addon">
+                id="button-addon2" onClick={pauseContract} >Set Paused</button>
+              <select className="form-select select-bg" id="inputGroupSelect03" 
+                aria-label="Example select with button addon" value={pauseValue} onChange={e=>setPauseValue(e.target.value)} >
               <option selected>true</option>
-              <option value="1">false</option>
+              <option defaultValue="1">false</option>
               </select>
             </div>
           <div className="input-group mb-3 px-4">
             <button className="btn btn-outline-secondary btns" type="button" 
-            id="button-addon2">Set Revealed</button>
-            <select className="form-select select-bg" id="inputGroupSelect03" aria-label="Example select with button addon">
+            id="button-addon2" onClick={revealContract}>Set Revealed</button>
+            <select className="form-select select-bg" value={revealValue} onChange={e=>setRevealValue(e.target.value)} 
+              id="inputGroupSelect03" aria-label="Example select with button addon">
               <option selected>true</option>
-              <option value="1">false</option>
+              <option defaultValue="1">false</option>
             </select>
           </div>
         </div>
         <div className='col-md-5 right-sec row'>
           <div className="input-group mb-3">
             <button className="btn btn-outline-secondary btns" type="button" 
-            id="button-addon2">Get Total Minted</button>
-            <p className='input-bg px-2 mint-user' >150</p>
+              id="button-addon2">Get Total Minted</button>
+            <p className='input-bg px-2 mint-user' >{}</p>
           </div>
           <div className="input-group mb-3">
             <button className="btn btn-outline-secondary btns" type="button" 
-            id="button-addon2">is Revealed?</button>
-            <p className='input-bg px-2 mint-user' >true</p>
+              onClick={getRevealedValue} id="button-addon2">Get Is Revealed</button>
+            <p className='input-bg px-2 mint-user' >{String(isRevealed)}</p>
           </div>
           <div className="input-group mb-3">
             <button className="btn btn-outline-secondary btns" type="button" 
-            id="button-addon2">is Paused?</button>
-            <p className='input-bg px-2 mint-user' >false</p>
+              onClick={getPausedValue}  id="button-addon2">Get Is Paused</button>
+            <p className='input-bg px-2 mint-user' >{String(isPaused)}</p>
           </div>
           <div className="input-group mb-3">
             <button className="btn btn-outline-secondary btns" type="button" 
-            id="button-addon2">Get Total Minted</button>
-            <p className='input-bg px-2 mint-user' >150</p>
+              onClick={getPreSaleValueAdmin}  id="button-addon2">Get Pre Sale Cost</button>
+            <p className='input-bg px-2 mint-user' >{String(preSaleCost)+' eth'}</p>
+          </div>
+          <div className="input-group mb-3">
+            <button className="btn btn-outline-secondary btns" type="button" 
+              onClick={getPublicSaleValueAdmin} id="button-addon2">Get Public Sale Cost</button>
+            <p className='input-bg px-2 mint-user' >{String(publicSaleCost)+' eth'}</p>
           </div>
           <div className="input-group mb-4">
             <button className="btn btn-outline-secondary withdraw-btn" type="button" 
-            id="button-addon2">Withdraw</button>
+              id="button-addon2">Withdraw</button>
           </div>
           <hr className='admin-mint-hr'/>
           <div className='container-fluid mx-0'>
@@ -91,14 +198,13 @@ const Admin = () => {
             <div className='d-flex justify-content-center'>
               <div className='d-flex flex-column admin-mint-opt align-items-center'>
                 <div>
-                  {/* <h3 className='text-center account-heading-text'>Mint by Only Paying Gas fee</h3> */}
                   <p className='text-white text-center mb-0 admin-mint-info'>Mint by only paying gas fees</p>
                   <div className='d-flex justify-content-center pt-4'>
-                    <button type="button"  className="btn btn-outline-primary mr-2 operator">+</button>
-                    <button type="button" className="btn btn-outline-primary admin-mint-btn">Mint</button>
-                    <button type="button"  className="btn btn-outline-primary ml-2 operator">-</button>
+                    <button type="button" onClick ={e=>increaseMint(e)}  className="btn btn-outline-primary mr-2 operator">+</button>
+                    <button type="button" onClick={e=>mintNfts(e)} className="btn btn-outline-primary admin-mint-btn">Mint</button>
+                    <button type="button" onClick ={e=>decreaseMint(e)} className="btn btn-outline-primary ml-2 operator">-</button>
                   </div>
-                  <p className='text-white text-center pt-2 admin-mint-info'>Mint Amount :</p>
+                  <p className='text-white text-center pt-2 admin-mint-info'>Mint Amount :{mint} </p>
                 </div> 
               </div>
             </div>
--- src/utils/contractUtils.js
+++ src/utils/contractUtils.js
@@ -1,12 +1,92 @@
 import Web3 from 'web3';
 import KarmaBear from '../artifacts/KarmaBears.json'
 
+
 export const web3Instance = new Web3(Web3.givenProvider || 
     'https://rinkeby.infura.io/v3/8f7bb415bf7b4dceb66c27ceea3bfb64','web3');
 
 export const contract = new web3Instance.eth.Contract(KarmaBear.output.abi,
     '0x369153A761d19bCa87881cD01AabD480c2ED5018')  
 
-export const isPaused = async () =>  { return contract.methods.paused().call()}
+export const getPaused = async () =>  { 
+    return await contract.methods.paused().call()
+}
 
+export const getRevealed = async () =>  { 
+    return await contract.methods.revealed().call()
+}
+
+export const getPublicSaleCost = async () =>  { 
+    return web3Instance.utils.fromWei(await contract.methods.publicSaleCost().call(), 'ether') 
+}
+
+export const getPreSaleCost = async () =>  { 
+    return web3Instance.utils.fromWei(await contract.methods.preSaleCost().call(), 'ether') 
+}
+
+export const getTotalMinted = async () =>  { 
+    return await contract.methods.totalMinted().call()
+}
+
+export const setPauseContract = async (value,address) =>  { 
+    return await contract.methods.setPaused(value).send({from: String(address)},(err)=>{
+        if(err !=undefined){
+            console.log(err,'err')
+            // toast.warn(err.message)
+        }
+    })
+}
+
+export const setRevealContract = async (value,address) =>  { 
+    return await contract.methods.setRevealed(value).send({from: String(address)},(err)=>{
+        if(err !=undefined){
+            console.log(err,'err')
+            // toast.warn(err.message)
+        }
+    })
+}
+
+export const setPreSaleCostContract = async (value,address) =>  { 
+    return await contract.methods.setPresaleCost(value).send({from: String(address)},(err)=>{
+        if(err !=undefined){
+            console.log(err,'err')
+            // toast.warn(err.message)
+        }
+    })
+}
+export const setPublicSaleCostContract = async (value,address) =>  { 
+    return await contract.methods.setPublicSaleCost(value).send({from: String(address)},(err)=>{
+        if(err !=undefined){
+            console.log(err,'err')
+            // toast.warn(err.message)
+        }
+    })
+}
+
+export const updateWhiteList = async (whiteListAddress,adminAddress) =>  { 
+    return await contract.methods.addUserToWhiteList(whiteListAddress).send({from: String(adminAddress)},(err)=>{
+        if(err !=undefined){
+            console.log(err,'err')
+            // toast.warn(err.message)
+        }
+    })
+}
+
+export const setBaseUri = async (uri,address) =>  { 
+    return await contract.methods.addUserToWhiteList(uri).send({from: String(address)},(err)=>{
+        if(err !=undefined){
+            console.log(err,'err')
+            // toast.warn(err.message)
+        }
+    })
+}
+
+export const mintByAdmin = async (quantity,address) =>  { 
+    return await contract.methods.adminMint(quantity).send({from: String(address),value:''},(err)=>{
+        if(err !=undefined){
+            console.log(err,'err')
+            // toast.warn(err.message)
+        }
+    })
+}
 
